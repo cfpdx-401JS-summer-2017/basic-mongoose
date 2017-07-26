bda3193181fa0843f49cd72c108bdd8e9234d72c
Mark Petrie <petrie.mark@gmail.com>
2017-07-26 13:08:24 -0700

    Initial setup, initial model and first passing model test

---
+++ tests/unit/customer.test.js
@@ -0,0 +1,15 @@
+const Customer = require('../../lib/models/customer');
+const { assert } = require('chai');
+
+describe('Customer model', () => {
+    it('validates gold path with required fields', () => {
+        const customer = new Customer({
+            name: 'Sally Hampton',
+            company: 'ABC Company',
+            tier:
+            { name: 'Bronze' }
+
+        });
+        return customer.validate();
+    });
+});

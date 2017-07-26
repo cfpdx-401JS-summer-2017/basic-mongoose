8b74f13c1083264e64a4e0c3174088869875bd2b
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-25 18:01:42 -0700

    created e2e otter test

--- tests/e2e/otters.test.js
+++ tests/e2e/otters.test.js
@@ -17,3 +17,88 @@ const app = require('../lib/app');
 // let chaiHttp start the server for us
 const request = chai.request(app);
 
+describe('otter REST api', () => {
+
+    // start with a clean slate, empty db
+    // for mongoose, use special dropDatabase directly on connection
+    before(() => connection.dropDatabase());
+
+    const benjamin = {
+        name: 'Benjamin',
+        type: 'river',
+        color: 'red',
+        food: [
+            { name: 'whipped cream', lbs: 1 }
+        ]
+    };
+
+    const juju = {
+        name: 'Juju',
+        type: 'angry',
+        color: 'eggshell',
+        food: [
+            { name: 'calamari olives', lbs: 19 },
+            { name: 'nickels', lbs: 8}
+        ]
+    };
+
+    const sonja = {
+        name: 'Sonja',
+        type: 'sea',
+        color: 'technicolor',
+        food: [
+            { name: 'cigarettes', lbs: 12 },
+            { name: 'otter pops', lbs: 250 },
+            { name: 'krill', lbs: 0.3}
+        ]
+    };
+
+    function saveOtter(otter) {
+        return request.post('/otters')
+            .send(otter)
+            .then(({ body }) => {
+                otter._id = body._id;
+                otter.__v = body.__v;
+                return body;
+            });
+    }
+
+    it('saves an otter', () => {
+        return saveOtter(benjamin)
+            .then(savedOtter => {
+                assert.isOk(savedOtter._id);
+                assert.deepEqual(savedOtter, benjamin);
+            });
+    });
+
+    it('GETs otter if it exists', () => {
+        return request
+            .get(`/otters/${benjamin._id}`)
+            .then(res => res.body)
+            .then(otter => assert.deepEqual(otter, benjamin));
+    });
+
+    it('returns 404 if otter does not exist', () => {
+        return request.get('/otters/58ff9f496aafd447254c29b5').then(
+            () => {
+                throw new Error('successful status code not expected');
+            },
+            res => {
+                assert.equal(res.status, 404);
+                assert.isOk(res.response.body.error);
+            }
+        );
+    });
+
+    it('GET all otters', () => {
+        return Promise.all([
+            saveOtter(juju),
+            saveOtter(sonja),
+        ])
+            .then(() => request.get('/otters'))
+            .then(res => {
+                const otters = res.body;
+                assert.deepEqual(otters, [benjamin, juju, sonja]);
+            });
+    });
+});

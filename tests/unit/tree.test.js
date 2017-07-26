cea5362d98a61524fc94d5603aef5b658f57af0e
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-07-26 12:27:54 -0700

    Added extra unit test for number type

--- tests/unit/tree.test.js
+++ tests/unit/tree.test.js
@@ -6,9 +6,7 @@ describe('Tree model', () => {
         const testTree = new Tree({
             variety: 'Aspen',
             type: 'deciduous',
-            location: [
-                { current: 'Portland', past: [{ previous: 'Eugene' }, { others: 'Bend' }] },
-            ]
+            locations: ['Portland', 'Bend']
         });
         return testTree.validate();
     });
@@ -22,17 +20,37 @@ describe('Tree model', () => {
             ({ errors }) => {
                 assert.ok(errors.variety);
                 assert.ok(errors.type);
-            }
-            );
+            });
     });
 
     it('type should be of enum type', () => {
         const testTree = new Tree({
-            variety: 'pine',
-            type: 'wrongType',
-            location: [
-                { current: 'Portland', past: [{ previous: 'Eugene' }, { others: 'Bend' }] },
-            ]
+            variety: 'Maple',
+            type: 'fake',
+            locations: ['Kyoto', 'Yokohama'],
+            ageEstimate: {
+                min: 50,
+                max: 75
+            }
+        });
+
+        return testTree.validate()
+            .then(() => {
+                throw new Error('Expected validation error');
+            },
+            ({ errors }) => assert.equal(errors.type.kind, 'enum')
+            );
+    });
+
+    it('min should be of Number type', () => {
+        const testTree = new Tree({
+            variety: 'Maple',
+            type: 'deciduous',
+            locations: ['Kyoto', 'Yokohama'],
+            ageEstimate: {
+                min: 'fake',
+                max: 75
+            }
         });
 
         return testTree.validate()
@@ -40,10 +58,8 @@ describe('Tree model', () => {
                 throw new Error('Expected validation error');
             },
             ({ errors }) => {
-                assert.equal(errors.type.kind, 'enum');
-            });
+                assert.deepEqual(errors['ageEstimate.min'].message, 'Cast to Number failed for value "fake" at path "ageEstimate.min"');
+            }
+            );
     });
 });
-
-// add tests for location - current required
-// add test for age

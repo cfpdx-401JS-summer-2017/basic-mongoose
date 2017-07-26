8fcfa35968a224fd1502dc9a88f56fa5222a77d9
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-07-26 09:36:51 -0700

    tests fail

--- tests/e2e/otters.test.js
+++ tests/e2e/otters.test.js
@@ -6,14 +6,14 @@ const { assert } = chai;
 /* connect to the test database */
 process.env.MONGODB_URI = 'mongodb://localhost:27017/otters-test';
 // run the actual connect to db
-require('../lib/connect');
+require('../../lib/connect');
 
 // get a reference to the global connection, because we need to drop
 // the database prior to running our tests
 const connection = require('mongoose').connection;
 
 // require our app
-const app = require('../lib/app');
+const app = require('../../lib/app');
 // let chaiHttp start the server for us
 const request = chai.request(app);
 

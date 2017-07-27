17c56f52808605b3dbac34847886991f96cffbf6
Wendy Beck <webeck@gmail.com>
2017-07-27 10:19:07 -0700

    commits from last night

--- tests/e2e/e2e-person.tests.js
+++ tests/e2e/e2e-person.tests.js
@@ -5,30 +5,31 @@ const chaiHttp = require("chai-http");
 const mongoose = require("../../lib/connect");
 const connection = require("mongoose").connection;
 const connect = require("mongoose").connect;
-const Promise = require("bluebird");
 
 chai.use(chaiHttp);
 
-describe.skip("mongoose tests", () => {
+describe("mongoose tests", () => {
   const req = chai.request(app);
 
   before(() => connect);
   beforeEach(() => connection.dropDatabase());
 
   const seedPeople = [
-    { name: 'elizabeth', nose: 'tiny', heightInInches:50, likesRollerCoasters:true },
-    { name: 'joann', job: 'web dev' },
-    { name: 'elton', talent: 'piano', likesRollerCoasters:null},
-    {
-      name: 'oliver', quirks: [
-        { annoying: "clips toenails on transit" },
-        { healthy: "always buys rounds" }
-      ]
-    },
-    { name: "james", hobby: "collects subway tokens from around the world", heightInInches: 64}
+    // { name: 'elizabeth', nose: 'tiny', heightInInches:50, likesRollerCoasters:true },
+    { name: 'joann', job: 'web dev' }
+    // ,
+    // { name: 'elton', talent: 'piano', likesRollerCoasters:null},
+    // {
+    //   name: 'oliver', quirks: [
+    //     { annoying: "clips toenails on transit" },
+    //     { healthy: "always buys rounds" }
+    //   ]
+    // },
+    // { name: "james", hobby: "collects subway tokens from around the world", heightInInches: 64}
   ]
 
   function save(person) {
+    // console.log('PPP: ', person);
     return req.post('/people')
       .send(person)
       .then(res => {
@@ -37,18 +38,22 @@ describe.skip("mongoose tests", () => {
       });
   };
 
-  it("GET /people", () => {
-    return Promise.all(seedPeople.map((person) => {
+  it.only("GET /people", () => {
+    return Promise.all(seedPeople.map(person => {
       return save(person)
     }))
     .then(res => {
+      console.log('test: ',res)
       return req.get("/people")
-    .then(data => {
-      console.log(res.text);
-    })
+      .then(data => {
+        // console.log(res.text);
+      })
   });
 }),
+
 it("GET /people/:id", () => {}), it("POST /people", () => {}), it("DELETE /people/:id", () => {
     // { removed: <result> } T/F
   }), it("PUT /people/:id", () => {}), it("PATCH /people/:id", () => {});
 });
+
+// findById then update actually does a patch

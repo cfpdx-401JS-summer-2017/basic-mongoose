8e6bcf993a2042447155903b6f5c5f913c91f144
Mark Petrie <petrie.mark@gmail.com>
2017-09-26 13:14:38 -0700

    Add remaining test cases for e2e test. Update app.js to handle customer deletes and 404 on get by id.

--- tests/unit/customer.test.js
+++ tests/unit/customer.test.js
@@ -6,10 +6,23 @@ describe('Customer model', () => {
         const customer = new Customer({
             name: 'Sally Hampton',
             company: 'ABC Company',
-            tier: [
-                { name: 'Bronze' }
-            ]
+            tier: { 
+                name: 'Bronze' 
+            }
+            
         });
         return customer.validate();
+    });
+
+    it('fails validation when missing required fields', () => {
+        const customer = new Customer({});
+        return customer.validate()
+            .then(
+                () => { throw new Error('Expected validation error'); },
+                ({ errors }) => {
+                    assert.ok(errors.name);
+                    assert.ok(errors.company);
+                }
+            );
     });
 });
